e20ce99e3c962ddeffacba610e97851e5de9d64f
Ben Berry <hobershort@gmail.com>
2011-08-08 01:35:00 -0400

    Add comments and a minor tweak.

--- src/com/hobershort/thinglist/TaskActivity.java
+++ src/com/hobershort/thinglist/TaskActivity.java
@@ -38,7 +38,8 @@ public class TaskActivity extends ListActivity {
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
+	
+		//Find out which window we are
 		window = getIntent().getExtras().getInt("window");
 		
 		final Resources res = getResources();
@@ -49,6 +50,7 @@ public class TaskActivity extends ListActivity {
 		setListAdapter(adapter);
 		adapter.add(res.getString(R.string.create_new));
 		
+		//Load any previous items
 		try {
 			FileInputStream inf = openFileInput(Integer.toString(window));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(inf));
@@ -65,19 +67,20 @@ public class TaskActivity extends ListActivity {
 			e.printStackTrace();
 		}
 		
-		
-		
-		
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
 		registerForContextMenu(lv);
 		
+		//Set up the click listener for list items
 		lv.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position,
 					long id) {
 				String selected = (String) parent.getItemAtPosition(position);
+				
+				//If the selected item is "Create new..." then it pops up the create dialog, 
+				//otherwise the context menu.
 				if(selected.equals(res.getString(R.string.create_new))){
 					popUpAddDialog(context, null);
 				}
@@ -88,21 +91,26 @@ public class TaskActivity extends ListActivity {
 		});
 	}
 
+	//Inflate our context menu XML file
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
 		super.onCreateContextMenu(menu, v, menuInfo);
 		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.contextmenu, menu);
-		
+		inflater.inflate(R.menu.contextmenu, menu);		
 	}
 
 	public boolean onContextItemSelected(MenuItem item) {
+		//Get the info for the selected list item
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
 		TextView x = (TextView) info.targetView;
+		
+		//Figure out which context menu item was selected
 		switch(item.getItemId()){
 			case R.id.delete:
+				//If it was delete, delete the item!
 				adapter.remove((String) x.getText());
 				return true;
 			case R.id.edit:
+				//Otherwise, delete the item and let the user seem to edit the text
 				popUpAddDialog(x.getContext(), x.getText());
 				adapter.remove((String) x.getText());
 				return true;
@@ -113,8 +121,16 @@ public class TaskActivity extends ListActivity {
 	
 	private void popUpAddDialog(final Context context, CharSequence charSequence) {
 		AlertDialog.Builder alert = new AlertDialog.Builder(context);
-		alert.setTitle("New list item");
-		alert.setMessage("Enter new to-do item");
+		
+		if(charSequence == null){
+			alert.setTitle("New list item");
+			alert.setMessage("Enter new to-do item");
+		}
+		else {
+			alert.setTitle("Edit list item");
+			alert.setMessage("Edit list item");
+		}
+		
 		final EditText textInput = new EditText(context);
 		textInput.setText(charSequence);
 		alert.setView(textInput);
@@ -133,7 +149,7 @@ public class TaskActivity extends ListActivity {
 
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
+		// Write out the list entries
 		super.onDestroy();
 		try {
 			FileOutputStream fos = openFileOutput(Integer.toString(window), Context.MODE_PRIVATE);
@@ -141,6 +157,8 @@ public class TaskActivity extends ListActivity {
 			String item;
 			for(int i = 0; i < adapter.getCount(); i++){
 				item = adapter.getItem(i);
+				
+				//Make sure not to write out the "Create new..." item
 				if(item != getResources().getString(R.string.create_new)){
 					item += "\n";
 					outf.write(item.toCharArray());
